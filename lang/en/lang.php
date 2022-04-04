53d31d510d0e51c03b8f62c95f98a80518831947
Marc Jauvin <marc.jauvin@gmail.com>
2022-04-04 13:47:59 -0400

    just use strings

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,14 +1,12 @@
 <?php
 
-$name = 'Mandrill secret';
-
 return [
     'plugin_description' => 'Mandrill mail driver plugin',
 
     'fields' => [
         'mandrill_secret' => [
-            'label' => $name,
-            'comment' => 'Enter your ' . $name,
+            'label' => 'Mandrill secret',
+            'comment' => 'Enter your Mandrill secret',
         ],
     ],
 ];
