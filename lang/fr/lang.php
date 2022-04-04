441ecc1acbd1e5c16a071cd254f327226c0a3dd5
Marc Jauvin <marc.jauvin@gmail.com>
2022-04-04 13:46:55 -0400

    add french localizations

---
+++ lang/fr/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'plugin_description' => "Plugin qui ajoute l'envoi de courriel à travers Mandrill"
+
+    'fields' => [
+        'mandrill_secret' => [
+            'label' => 'Secret Mandrill',
+            'comment' => 'Entrez votre secret Mandrill',
+        ],
+    ],
+];
